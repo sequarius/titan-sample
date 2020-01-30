762fbbc3ff67a44f11643c3cd82b41d4cca6648e
sequarius <sequarius@gmail.com>
2020-01-30 18:10:41 +0800

    init: add project template

---
+++ titan-sample-app/src/main/java/com/sequarius/titan/sample/SampleApplication.java
@@ -0,0 +1,18 @@
+package com.sequarius.titan.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * project titan-sample
+ *
+ * @author Sequarius *
+ * @since 30/01/2020
+ */
+@SpringBootApplication
+public class SampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+}
---
+++ titan-sample-common/src/main/java/com/sequarius/common/Page.java
@@ -0,0 +1,15 @@
+package com.sequarius.common;
+
+import lombok.Data;
+
+/**
+ * project titan
+ *
+ * @author Sequarius *
+ * @since 12/01/2020
+ */
+@Data
+public class Page {
+    private Integer start;
+    private Integer length;
+}
