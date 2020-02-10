713f314e0f1d065ed26cfa012cd114cfebcd12b6
sequarius <sequarius@gmail.com>
2020-02-10 21:07:53 +0800

    update: jackson instance config. fixed: db connection encode.

--- titan-sample-common/src/main/java/com/sequarius/titan/sample/common/util/JacksonUtil.java
+++ titan-sample-common/src/main/java/com/sequarius/titan/sample/common/util/JacksonUtil.java
@@ -1,6 +1,8 @@
 package com.sequarius.titan.sample.common.util;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 /**
  * project titan-sample
@@ -9,8 +11,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * @since 01/02/2020
  */
 public class JacksonUtil {
+
+    private static ObjectMapper objectMapper;
+
     public static ObjectMapper getObjectMapper() {
-        return new ObjectMapper();
+        if (objectMapper == null) {
+            synchronized (JacksonUtil.class) {
+                if (objectMapper == null) {
+                    objectMapper = new ObjectMapper();
+                    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                    objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+                }
+            }
+        }
+        return objectMapper;
     }
 
 }
