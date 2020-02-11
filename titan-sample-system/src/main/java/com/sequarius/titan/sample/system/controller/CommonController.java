a8eda2958769c19f58bafa59c69c8912cece1074
sequarius <sequarius@gmail.com>
2020-02-11 14:32:02 +0800

    feature: add login get support for debug

--- titan-sample-system/src/main/java/com/sequarius/titan/sample/system/controller/CommonController.java
+++ titan-sample-system/src/main/java/com/sequarius/titan/sample/system/controller/CommonController.java
@@ -6,6 +6,7 @@ import com.sequarius.titan.sample.system.message.SystemMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,12 @@ public class CommonController {
 
     @Resource
     private SystemMessage systemMessage;
+
+    @GetMapping("/login")
+    public Response<String> debugLogin(LoginRequestDTO requestDTO){
+        return login(requestDTO);
+    }
+
 
     @PostMapping("/login")
     public Response<String> login(@Valid @RequestBody LoginRequestDTO requestDTO) {
